b9f7219aa116f00bc6467ee54cf5227c29dd5844
LiuJianjun <jianjun@dappworks.cn>
2018-09-19 09:47:39 +0800

    init basic file

---
+++ model/user.go
@@ -0,0 +1,8 @@
+package model
+
+type User struct {
+	Id       int    `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Status   int    `json:"status"`
+}
---
+++ service/handlers.go
@@ -0,0 +1,26 @@
+package service
+
+import (
+	"net/http"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	
+)
+func ResponseWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+
+func GetName(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type","application/json;charset=UTF-8")
+	w.Write([]byte("{\"name\":\"good job\"}"))
+}
+
+func GetAge(w http.ResponseWriter,r *http.Request){
+	age:=mux.Vars(r)["age"]
+	// w.Header().Set("Content-Type","application/json;charset=UTF-8")
+	w.Write([]byte("age is "+age))
+}
